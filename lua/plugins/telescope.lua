626fcc63e4d3e3ec3746b4abdb6598a260920bee
Xenon1019 <64680054+Xenon1019@users.noreply.github.com>
2025-08-22 18:31:27 +0000

    Added telescope dependency bug fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
     },
     {
         'nvim-telescope/telescope-ui-select.nvim',
+        dependencies = { 'nvim-telescope/telescope.nvim' },
         opts = {
             extensions = {
                 ["ui-select"] = {
